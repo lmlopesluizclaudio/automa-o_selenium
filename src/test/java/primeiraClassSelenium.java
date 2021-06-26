cb7d87384d9b2e219eb2e9be2113a5a2ef96c039
luiz claudio <llopes@server01.foursys.local>
2021-06-26 18:00:32 -0300

    primeira classe selemiun

---
+++ src/test/java/primeiraClassSelenium.java
@@ -0,0 +1,16 @@
+
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class primeiraClassSelenium {
+
+ @Test
+public void testeLogin() { 
+	
+	ChromeDriver driver = new ChromeDriver();
+	
+	driver.get("https://www.amazon.com.br/");
+}
+
+
+}
